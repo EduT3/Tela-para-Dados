87a14002dcc1c59e3eec35b8f6c436b3e75214a4
Eduardo Paulo Tomilhero <Eduardo_tomi@hotmail.com>
2023-11-24 20:04:46 -0300

    guardar informações
    guardar localmente as informações e ler além de poder remover

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var dadosArmazenados = []; // Cria um array vazio
+// Cria um array vazio ou recupera do localStorage se existir
+var dadosArmazenados = JSON.parse(localStorage.getItem('dados')) || [];
 
 function enviarDados(event) {
     event.preventDefault(); // Impede que o formulário seja enviado
@@ -20,6 +21,9 @@ function enviarDados(event) {
     // Adiciona o objeto de dados ao array
     dadosArmazenados.push(dados);
 
+    // Armazena o array atualizado no localStorage
+    localStorage.setItem('dados', JSON.stringify(dadosArmazenados));
+
     console.log("Seus dados de acesso são\n" + "Email:" +
         dados.email +
         "\nSenha: " + dados.senha +
@@ -27,41 +31,63 @@ function enviarDados(event) {
         "\nNacionalidade: " + dados.nacionalidade);
 
     // Exibe os dados na tela
-    function exibirDados() {
-        var dadosExibidos = document.getElementById("dados-exibidos");
-      
-        // Limpa os dados anteriores exibidos
-        dadosExibidos.innerHTML = "";
-      
-        // Percorre o array de dados e exibe cada objeto
-        for (var i = 0; i < dadosArmazenados.length; i++) {
-          var dados = dadosArmazenados[i];
-      
-          // Cria um elemento de lista <ul> para cada conjunto de dados
-          var listaDados = document.createElement("ul");
-      
-          // Cria um elemento <li> para cada campo de dados
-          var emailItem = document.createElement("li");
-          emailItem.textContent = "E-mail: " + dados.email;
-          listaDados.appendChild(emailItem);
-      
-          var senhaItem = document.createElement("li");
-          senhaItem.textContent = "Senha: " + dados.senha;
-          listaDados.appendChild(senhaItem);
-      
-          var telefoneItem = document.createElement("li");
-          telefoneItem.textContent = "Telefone: " + dados.telefone;
-          listaDados.appendChild(telefoneItem);
-      
-          var nacionalidadeItem = document.createElement("li");
-          nacionalidadeItem.textContent = "Nacionalidade: " + dados.nacionalidade;
-          listaDados.appendChild(nacionalidadeItem);
-      
-          // Adiciona a lista de dados ao elemento HTML
-          dadosExibidos.appendChild(listaDados);
-        }
-
-      }
-      
-      exibirDados();
+    exibirDados();
 }
+
+function exibirDados() {
+    var dadosExibidos = document.getElementById("dados-exibidos");
+
+    // Limpa os dados anteriores exibidos
+    dadosExibidos.innerHTML = "";
+
+    // Percorre o array de dados e exibe cada objeto
+    for (var i = 0; i < dadosArmazenados.length; i++) {
+        var dados = dadosArmazenados[i];
+
+        // Cria um elemento de lista <ul> para cada conjunto de dados
+        var listaDados = document.createElement("ul");
+
+        // Cria um elemento <li> para cada campo de dados
+        var emailItem = document.createElement("li");
+        emailItem.textContent = "E-mail: " + dados.email;
+        listaDados.appendChild(emailItem);
+
+        var senhaItem = document.createElement("li");
+        senhaItem.textContent = "Senha: " + dados.senha;
+        listaDados.appendChild(senhaItem);
+
+        var telefoneItem = document.createElement("li");
+        telefoneItem.textContent = "Telefone: " + dados.telefone;
+        listaDados.appendChild(telefoneItem);
+
+        var nacionalidadeItem = document.createElement("li");
+        nacionalidadeItem.textContent = "Nacionalidade: " + dados.nacionalidade;
+        listaDados.appendChild(nacionalidadeItem);
+
+        // Adiciona um botão para remover este conjunto de dados
+        var removerBotao = document.createElement("button");
+        removerBotao.textContent = "Remover";
+        removerBotao.dataset.index = i; // Armazena o índice no dataset para identificar qual conjunto de dados remover
+        removerBotao.addEventListener("click", removerDados);
+        listaDados.appendChild(removerBotao);
+
+        // Adiciona a lista de dados ao elemento HTML
+        dadosExibidos.appendChild(listaDados);
+    }
+}
+
+function removerDados(event) {
+    var index = event.target.dataset.index;
+
+    // Remove o conjunto de dados do array
+    dadosArmazenados.splice(index, 1);
+
+    // Atualiza o localStorage
+    localStorage.setItem('dados', JSON.stringify(dadosArmazenados));
+
+    // Exibe os dados atualizados na tela
+    exibirDados();
+}
+
+// Chama exibirDados quando a página é carregada para mostrar os dados salvos
+window.onload = exibirDados;
